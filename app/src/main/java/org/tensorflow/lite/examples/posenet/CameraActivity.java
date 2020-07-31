b55080d2ec3c52a17491d1fd6bcb35d8490a5326
Noah Weiner <noah.weiner@yale.edu>
2020-07-30 20:35:54 -0400

    kotlin-java translation

--- app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActivity.java
@@ -1,30 +1,12 @@
-/*
- * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.tensorflow.lite.examples.posenet;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-
-public class CameraActivity extends Fragment {
+public class CameraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,12 +16,10 @@ public class CameraActivity extends Fragment {
         setContentView(R.layout.tfe_pn_activity_camera);
         Toast.makeText(getApplicationContext(), "Welcome to CameraActivity.kt", Toast.LENGTH_SHORT).show();
 
-
         //add the PoseNet submodule into the activity
         if (savedInstanceState==null) {
-            /*FragmentManager*/getSupportFragmentManager()./*FragmentTransaction*/beginTransaction().
-            replace(R.id.container, PosenetActivity.class).commit();
+            /*FragmentManager*/getSupportFragmentManager()./*FragmentTransaction*/beginTransaction()
+                    .replace(R.id.container, new PosenetActivity()).commit();
         }
     }
-
 }
